c9bb4d9b30ff8fdc539b2b0a765137f1932fefd8
Nicholas Marriott <nicholas.marriott@gmail.com>
2005-12-01 22:19:23 +0000

    Take note of environment.

--- make-linux.sh
+++ make-linux.sh
@@ -1,34 +1,36 @@
 #!/bin/sh
 
-set -x
-
 if [ "$1" == "clean" ]; then
     rm -f logfmon *.o y.tab.c lex.yy.c y.tab.h *~
     exit
 fi
 
-CC="gcc"
-# GCC 4 sucks, use GCC 3.x if possible.
-if which gcc-3.3 1>/dev/null 2>&1; then
-    CC="gcc-3.3"
-fi
-if which gcc-3.4 1>/dev/null 2>&1; then
-    CC="gcc-3.4"
+if [ -z "$CC" ]; then
+    CC="gcc"
+    
+    # Some versions of GCC 4 appear to be weird. Use 3 if possible.
+    if which gcc-3.3 1>/dev/null 2>&1; then
+	CC="gcc-3.3"
+    fi
+    if which gcc-3.4 1>/dev/null 2>&1; then
+	CC="gcc-3.4"
+    fi
 fi
 
 CFLAGS="-I- -I. -I/usr/local/include $CFLAGS \
 	-D_GNU_SOURCE -D_LARGEFILE_SOURCE \
         -Wall -W -Wmissing-prototypes -Wmissing-declarations \
         -Wshadow -Wpointer-arith -Wcast-qual -Wsign-compare"
-LDFLAGS="-L/usr/local/lib"
+LDFLAGS="-L/usr/local/lib $LDFLAGS"
 LIBS="-lm -lpthread"
 
-YACC="bison -d -y"
-LEX="lex"
+[ -z "$YACC" ] && YACC="bison -d -y"
+[ -z "$LEX" ] && LEX="lex"
 
 [ ! -f y.tab.c ] && $YACC parse.y
 [ ! -f lex.yy.c ] && $LEX lex.l
 
+set -x
 SRCS=`echo *.c| sed -e s'/event.c//'`
 for i in $SRCS; do
     [ ! -f ${i%.c}.o ] && $CC $CFLAGS -c $i -o ${i%.c}.o
