f3e7352a5c291ced30b55e6650ff83d819db3e1d
Nicholas Marriott <nicholas.marriott@gmail.com>
2005-12-01 22:10:55 +0000

    +logfmon.h

--- strlcpy.c
+++ strlcpy.c
@@ -20,6 +20,8 @@
 #include <sys/types.h>
 #include <string.h>
 
+#include "logfmon.h"
+
 /*
  * Copy src to string dst of size siz.  At most siz-1 characters
  * will be copied.  Always NUL terminates (unless siz == 0).
