d8c409f4c14da8a00ac91c305c78dac69e4ad2f3
Nicholas Marriott <nicholas.marriott@gmail.com>
2006-11-08 14:16:19 +0000

    Use /tmp to avoid NFS.

--- regress/test-symlink.sh
+++ regress/test-symlink.sh
@@ -2,44 +2,47 @@
 # $Id$
 
 # Sleep time
-TIME=3
+TIME=5
+
+# File prefix
+P=/tmp/test-symlink
 
 # Create the config file
-cat <<EOF >test-symlink.conf
+cat <<EOF >${P}.conf
 set mailcmd ""
 set logregexp "(.*)"
-file "test-symlink.link" tag test
+file "${P}.link" tag test
 match in test ".*" ignore
 EOF
 
 # Create the log file
-ln -sf test-symlink.file test-symlink.link || exit 1
-touch test-symlink.file || exit 1
+ln -sf ${P}.file ${P}.link || exit 1
+touch ${P}.file || exit 1
 
 # Start logfmon
-../logfmon -d -f test-symlink.conf -c '' -p '' >test-symlink.out 2>&1 &
+../logfmon -d -f ${P}.conf -c '' -p '' >${P}.out 2>&1 &
 
 # Initial test
 echo -n .; sleep $TIME
-echo test-1 >test-symlink.file
+echo test-1 >${P}.file
 echo -n .; sleep $TIME
-echo test-2 >>test-symlink.file
+echo test-2 >>${P}.file
 echo -n .; sleep $TIME
 
 # Test remove
-rm test-symlink.file || exit 1
+rm ${P}.file || exit 1
 echo -n .; sleep $TIME
-echo test-3 >test-symlink.file
+echo test-3 >${P}.file
 echo -n .; sleep $TIME
-echo test-4 >>test-symlink.file
+echo test-4 >>${P}.file
 echo -n .; sleep $TIME
 
 # Test move
-mv test-symlink.file test-symlink.moved || exit 1
+mv ${P}.file ${P}.moved || exit 1
 echo -n .; sleep $TIME
-echo test-5 >test-symlink.file
+echo test-5 >${P}.file
 echo -n .; sleep $TIME
-echo test-6 >>test-symlink.file
+echo test-6 >>${P}.file
 echo -n .; sleep $TIME
 
 # Kill logfmon
@@ -47,7 +50,7 @@ echo
 kill %1
 
 # Cat the output
-cat test-symlink.out || exit 1
+cat ${P}.out || exit 1
 
 # Clean up
-rm -f test-symlink.conf test-symlink.out test-symlink.file test-symlink.link test-symlink.moved
+rm -f ${P}.conf ${P}.out ${P}.file ${P}.link ${P}.moved
